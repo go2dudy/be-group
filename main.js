370e638348e4833864d710394dd896c5fba93dba
go2dudy <47638194+go2dudy@users.noreply.github.com>
2019-02-14 19:02:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var video = document.getElementById("myVideo");
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
